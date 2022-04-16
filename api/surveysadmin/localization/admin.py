fd352ece7356bb5e1bfa1e04ea69403df0cb9a3f
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:30:45 -0500

    feat: added statedepartment and city to admin

--- api/surveysadmin/localization/admin.py
+++ api/surveysadmin/localization/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from api.surveysadmin.localization.models import Country
+from api.surveysadmin.localization.models import Country, StateDepartment, City
 
 
 @admin.register(Country)
-class CompanyAdmin(admin.ModelAdmin):
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(StateDepartment)
+class StateDepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class City(admin.ModelAdmin):
     pass
