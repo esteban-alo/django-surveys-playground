9a21548d09df6a94f7369d766ffc4e00ac8be881
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:35:04 -0500

    feat: updated localization models.py

--- api/surveysadmin/localization/models.py
+++ api/surveysadmin/localization/models.py
@@ -1,43 +1,55 @@
 from django.db import models
 
-from api.surveysadmin.commons.models import CommonModel
+from api.surveysadmin.commons.models import (
+    CommonFieldsModel,
+    CommonOptionTypesModel,
+    NameField,
+)
 
 
 # Create your models here.
-class Country(CommonModel):
-    name = models.CharField(max_length=56, unique=True)
+class PoliticalSystemTypes(CommonOptionTypesModel):
+    class Meta:
+        db_table = "localization_political_system"
+        verbose_name_plural = "Political System "
+
+    def __str__(self) -> str:
+        return f"{self.type_name}"
+
+
+class Country(CommonFieldsModel):
+    name = NameField(max_length=56, unique=True)
     iso_name_2 = models.CharField(max_length=2, unique=True)
     iso_name_3 = models.CharField(max_length=3, unique=True)
     phone_code = models.CharField(max_length=7, unique=True)
-
+    political_system = models.ForeignKey(PoliticalSystemTypes, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "Countries"
 
     def __str__(self) -> str:
-        return f"self.name"
+        return f"{self.name}"
 
 
-class State_Department(CommonModel):
+class StateDepartment(CommonFieldsModel):
 
-    name = models.CharField(max_length=56, unique=True)
-    iso_2 = models.CharField(max_length=2, unique=True)
-    iso_3 = models.CharField(max_length=3, unique=True)
-    countries = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = NameField(max_length=56, unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     class Meta:
+        db_table = "localization_state_department"
         verbose_name_plural = "States or Departments"
 
     def __str__(self) -> str:
-        return f"self.name"
+        return f"{self.name}"
 
 
-class City(CommonModel):
+class City(CommonFieldsModel):
 
-    name = models.CharField(max_length=56, unique=True)
+    name = NameField(max_length=56, unique=True)
     iso_name_2 = models.CharField(max_length=2, unique=True)
     iso_name_3 = models.CharField(max_length=3, unique=True)
-    state_departments = models.ForeignKey(State_Department, on_delete=models.CASCADE)
+    state_department = models.ForeignKey(StateDepartment, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "Cities"
