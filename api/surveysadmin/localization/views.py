b6d2f3cb93ecabffc7625e2d566cc5d2478ba889
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:38:11 -0500

    feat: updated localization views

--- api/surveysadmin/localization/views.py
+++ api/surveysadmin/localization/views.py
@@ -3,12 +3,27 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.mixins import RetrieveModelMixin
 
-from api.surveysadmin.localization.models import Country
-from api.surveysadmin.localization.serializers import CountrySerializer
+from api.surveysadmin.localization.models import Country, StateDepartment, City
+from api.surveysadmin.localization.serializers import (
+    CountrySerializer,
+    StateDepartmentSerializer,
+)
 
 
 # Create your views here.
 class CountryViewSet(ReadOnlyModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all().order_by("-updated_date")
-    pagination_class = PageNumberPagination
+    pagination = PageNumberPagination
+
+
+class StateDepartmentViewSet(ReadOnlyModelViewSet):
+    queryset = StateDepartment.objects.all().order_by("-updated_date")
+    serializer_class = StateDepartmentSerializer
+    pagination = PageNumberPagination
+
+
+class CityViewSet(ReadOnlyModelViewSet):
+    queryset = StateDepartment.objects.all().order_by("-updated_date")
+    serializer_class = City
+    pagination = PageNumberPagination
