a60e68b0231c88558a52c98d993666d8f3d395ed
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:34:06 -0500

    feat: created new common models

--- api/surveysadmin/commons/models.py
+++ api/surveysadmin/commons/models.py
@@ -2,11 +2,32 @@ from uuid import uuid4
 from django.db import models
 
 
-class CommonModel(models.Model):
+class NameField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super(NameField, self).__init__(*args, **kwargs)
 
-    created_date = models.DateTimeField(auto_now_add=True)
+    def get_prep_value(self, value):
+        return str(value).title()
+
+
+class CommonFieldsModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, editable=False)
     updated_date = models.DateTimeField(auto_now=True)
     uuid = models.UUIDField(default=uuid4(), editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class CommonFormsModel(CommonFieldsModel):
+    is_enabled = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class CommonOptionTypesModel(CommonFieldsModel):
+    type_name = models.CharField(max_length=150)
 
     class Meta:
         abstract = True
