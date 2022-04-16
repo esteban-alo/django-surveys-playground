d17834535daed0c5ac8fde5b69fa00e6bd2964c6
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:37:08 -0500

    feat: updated localization registered urls

--- api/surveysadmin/localization/urls.py
+++ api/surveysadmin/localization/urls.py
@@ -1,6 +1,26 @@
 from rest_framework import routers
 
-from api.surveysadmin.localization.views import CountryViewSet
+from api.surveysadmin.localization.views import (
+    CountryViewSet,
+    StateDepartmentViewSet,
+    CityViewSet,
+)
 
-countries_router = routers.DefaultRouter()
-countries_router.register("localization", viewset=CountryViewSet, basename="localization")
+localization_router = routers.DefaultRouter()
+localization_router.register(
+    "localization/country",
+    viewset=CountryViewSet,
+    basename="country",
+)
+
+localization_router.register(
+    "localization/state_department",
+    viewset=StateDepartmentViewSet,
+    basename="state",
+)
+
+localization_router.register(
+    "localization/city",
+    viewset=CityViewSet,
+    basename="city",
+)
