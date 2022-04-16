3ced5e29e7dd15eebe3456332bfc5b37c582355e
esteban-alo <estebanrodalo@gmail.com>
2022-04-15 21:36:29 -0500

    feat: updated installed_apps components

--- api/surveysadmin/surveysadmin/settings.py
+++ api/surveysadmin/surveysadmin/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "api.surveysadmin.form",
     "api.surveysadmin.localization",
 ]
 
@@ -74,8 +75,12 @@ WSGI_APPLICATION = "surveysadmin.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "surveys-playground",
+        "USER": "postgres",
+        "PASSWORD": "mysecretpassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
